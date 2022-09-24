e4bb1ac22beee54d27273fe30b94abd83c549156
serj-43 <serj-43@yandex.ru>
2022-09-24 19:03:26 +0300

    fixies according to comments

--- src/test/java/PlanMeetingTest.java
+++ src/test/java/PlanMeetingTest.java
@@ -8,11 +8,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PlanMeetingTest {
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @BeforeEach
     void initOperations() {
@@ -21,16 +24,16 @@ public class PlanMeetingTest {
 
     @Test
     void shouldPlanMeeting() {
-        String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String planDate = generateDate(3);
         $x("//*[@data-test-id='city']//input").setValue("Киров");
         $x("//*[@data-test-id='date']//input").sendKeys(Keys.CONTROL + "A" + Keys.BACK_SPACE);
-        $x("//*[@data-test-id='date']//input").setValue(date);
+        $x("//*[@data-test-id='date']//input").setValue(planDate);
         $x("//*[@data-test-id='name']//input").setValue("Иван Петров-Сидоров");
         $x("//*[@data-test-id='phone']//input").setValue("+79536992752");
         $x("//*[@data-test-id='agreement']").click();
         $x("//button//*[text()='Забронировать']").click();
         $x("//*[@data-test-id='notification']//*[@class='notification__title'][text()='Успешно!']").should(visible, Duration.ofSeconds(20));
-        $x("//*[@data-test-id='notification']//*[@class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + date));
+        $x("//*[@data-test-id='notification']//*[@class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + planDate));
     }
 
     @Test
@@ -38,27 +41,17 @@ public class PlanMeetingTest {
         $x("//*[@data-test-id='city']//input").setValue("Ки");
         $x("//*[@class='popup__container']//*[@class='menu-item__control'][text()='Киров']").click();
         $x("//*[contains(@class,'calendar-input')]//button").click();
-        int day = Integer.parseInt(($x("//table[@class='calendar__layout']//td[contains(@class,'calendar__day_state_today')]").getText()));
-
-        int i = 0;
-        String maxDay = "";
-        while (maxDay.equals("") && i < 7) {
-            maxDay = $x("//table[@class='calendar__layout']//tr[last()]//td[last() -" + i + "]").getText();
-            i++;
-        }
-        if (maxDay == "") maxDay = $x("//table[@class='calendar__layout']//tr[last()-1]//td[last() -" + i + "]").getText();
-
-        if ((day + 7) > Integer.parseInt(maxDay)) $x("//*[@data-step=\"1\"]").click();
-        day = (day + 7) % Integer.parseInt(maxDay);
-
-        $x("//table[@class='calendar__layout']//td[text()='" + day + "']").click();
+        LocalDate currentDate = LocalDate.now().plusDays(3);
+        LocalDate planDate = LocalDate.now().plusDays(7);
+        if (currentDate.getMonth() != planDate.getMonth()) $x("//*[@data-step=\"1\"]").click();
+        String day = String.valueOf(planDate.getDayOfMonth());
+        $$x("//*[contains(@class,'calendar__day')]").findBy(text(day)).click();
         $x("//*[@data-test-id='name']//input").setValue("Иван Петров-Сидоров");
         $x("//*[@data-test-id='phone']//input").setValue("+79536992752");
         $x("//*[@data-test-id='agreement']").click();
         $x("//button//*[text()='Забронировать']").click();
         $x("//*[@data-test-id='notification']//*[@class='notification__title'][text()='Успешно!']").should(visible, Duration.ofSeconds(20));
-        String date = $x("//*[@data-test-id=\"date\"]//*[@class='input__control']").getValue();
-        $x("//*[@data-test-id='notification']//*[@class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + date));
+        $x("//*[@data-test-id='notification']//*[@class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + generateDate(7)));
     }
 
 }
